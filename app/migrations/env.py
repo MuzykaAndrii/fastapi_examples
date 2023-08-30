0e8df664fec4d08c0ebfa86207933a782b259000
andrymyzik <andrymyzik@gmail.com>
2023-08-30 20:50:33 +0300

    created and executed migration

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from config import (
     DB_HOST,
     DB_NAME,
+    DB_USER,
     DB_PASS,
     DB_PORT,
 )
@@ -20,9 +21,10 @@ config = context.config
 
 section = config.config_ini_section
 config.set_section_option(section, "DB_HOST", DB_HOST)
+config.set_section_option(section, "DB_PORT", DB_PORT)
+config.set_section_option(section, "DB_USER", DB_USER)
 config.set_section_option(section, "DB_NAME", DB_NAME)
 config.set_section_option(section, "DB_PASS", DB_PASS)
-config.set_section_option(section, "DB_PORT", DB_PORT)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
