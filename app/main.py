d07014749682df34559598ce0e478544fc67eca5
andrymyzik <andrymyzik@gmail.com>
2023-09-01 14:36:20 +0300

    implemented register route, changed startup point

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from fastapi_users import FastAPIUsers, fastapi_users
+
+from app.auth.database import User
+from app.auth.auth import auth_backend
+from app.auth.manager import get_user_manager
+from app.auth.schemas import UserRead, UserCreate
 
 
 app = FastAPI(
@@ -6,6 +12,20 @@ app = FastAPI(
 )
 
 
-@app.get('/', response_model=list[str])
-def hello():
-    return ["Hello, world"]
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
