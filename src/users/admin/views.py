afad1363fe4790aaa504cc138a4cc743a50ea65e
MuzykaAndrii <andrymyzik@gmail.com>
2023-09-19 16:54:53 +0300

    improved admin

--- src/users/admin/views.py
+++ src/users/admin/views.py
@@ -1,6 +1,7 @@
 from starlette_admin.contrib.sqla.ext.pydantic import ModelView
-from users.admin.schemas import RoleAdminSchema, UserAdminSchema
+from starlette_admin import ExportType
 
+from users.admin.schemas import RoleAdminSchema, UserAdminSchema
 from users.models import Role, User
 
 
@@ -18,6 +19,23 @@ class UserAdminView(ModelView):
     exclude_fields_from_list = ["hashed_password"]
     exclude_fields_from_detail = ["hashed_password"]
     exclude_fields_from_edit = ["hashed_password"]
+
+    export_fields = [
+        "id",
+        "username",
+        "email",
+        "registered_at",
+    ]
+    export_types = [
+        ExportType.EXCEL,
+        ExportType.PDF,
+        ExportType.PRINT,
+    ]
+
+    column_visibility = True
+    search_builder = True
+    responsive_table = True
+    save_state = True
 
 
 class RoleAdminView(ModelView):
